fed60794b6bc37a531df3deadabe09ba05d6d919
Lucas Voltera <lucasvoltera@gmail.com>
2023-12-26 18:35:54 -0300

    add home page with bootrstrap

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from app import views
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+app.add_url_rule(rule='/', endpoint='home', view_func=views.index)
 
 if __name__ == '__main__':
     app.run(debug=True)
